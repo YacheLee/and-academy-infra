2f01eb65b792eee5913b58842312d449478a2b4c
ya-che.li <ya-che.li@and.digital>
2020-02-18 09:49:02 +0000

    commit for terraform

---
+++ dns.tf
@@ -0,0 +1,34 @@
+variable "dns" {
+  default = "ya.dekker-and.digital"
+}
+
+resource "aws_route53_zone" "main" {
+  name = var.dns
+}
+
+output "main_name_servers" {
+  value = aws_route53_zone.main.name_servers
+}
+
+resource "aws_route53_record" "main-txt" {
+  zone_id = aws_route53_zone.main.id
+  name = var.dns
+  type = "TXT"
+  records = [
+    "test=abc", # more text records can be added below
+  ]
+  ttl = 900
+}
+
+resource "aws_route53_record" "ya-che" {
+  name = "ya.dekker-and.digital"
+  zone_id = aws_route53_zone.club-dns.id
+  type = "NS"
+  ttl = 900
+  records = [
+      "ns-1654.awsdns-14.co.uk.",
+      "ns-1085.awsdns-07.org.",
+      "ns-995.awsdns-60.net.",
+      "ns-129.awsdns-16.com."
+  ]
+}
