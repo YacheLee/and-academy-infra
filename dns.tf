8cbdfef650a0341055c7553b49e1f09ef23411b7
ya-che.li <ya-che.li@and.digital>
2020-02-18 10:18:12 +0000

    fix bug for ya.aa.dekker-and.digital

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 variable "dns" {
-  default = "ya.dekker-and.digital"
+  default = "aa.dekker-and.digital"
 }
 
 resource "aws_route53_zone" "main" {
@@ -20,15 +20,16 @@ resource "aws_route53_record" "main-txt" {
   ttl = 900
 }
 
-resource "aws_route53_record" "ya-che" {
-  name = "ya.dekker-and.digital"
+
+resource "aws_route53_record" "alex-zone" {
+  name = "ya.${var.dns}"
   zone_id = aws_route53_zone.main.id
   type = "NS"
   ttl = 900
   records = [
-      "ns-1654.awsdns-14.co.uk.",
-      "ns-1085.awsdns-07.org.",
-      "ns-995.awsdns-60.net.",
-      "ns-129.awsdns-16.com."
+    "ns-1379.awsdns-44.org",
+    "ns-1583.awsdns-05.co.uk",
+    "ns-43.awsdns-05.com",
+    "ns-716.awsdns-25.net"
   ]
 }
